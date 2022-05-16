32e8a621ac33a7c0290d8974eb7f2506b535033d
glennmateus <glennmateus@gmail.com>
2022-05-16 20:37:57 -0300

    initial commit

---
+++ LogFileConverter/Entities/DirectoryFunctions.cs
@@ -0,0 +1,15 @@
+﻿namespace LogFileConverter.Entities;
+
+public struct DirectoryFunctions
+{
+    public static void CreateDirectoryIfNotExists(string path)
+    {
+        var pathWithoutFileName = string.Join('/',
+            path.Split('/').Where(c => !c.EndsWith(".txt")).ToList()
+        );
+        if (!Directory.Exists(pathWithoutFileName))
+        {
+            Directory.CreateDirectory(pathWithoutFileName);
+        }
+    }
+}
---
+++ LogFileConverter/Entities/LogFileFromURI.cs
@@ -0,0 +1,12 @@
+﻿namespace LogFileConverter.Entities;
+
+public partial class LogFile
+{
+    public static Stream GetFromUri(string uri)
+    {
+        var request = new HttpClient()
+            .GetAsync(uri)
+            .Result;
+        return request.Content.ReadAsStream();
+    }
+}
